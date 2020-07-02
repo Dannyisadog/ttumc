8a663f35d4fa839343e850262752fbe6dedcd559
Dannyisadog <dannyisadog10@gmail.com>
2020-07-02 23:02:46 +0800

    adjust main page color and navbar style

--- public/js/app.js
+++ public/js/app.js
@@ -49832,15 +49832,27 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/layouts.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/layouts.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/layouts.scss ***!
+  \*******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Users/danny/Documents/common/coding/php/ttumc/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/danny/Documents/common/coding/php/ttumc/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/danny/Documents/common/coding/php/ttumc/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/danny/Documents/common/coding/php/ttumc/resources/sass/layouts.scss */"./resources/sass/layouts.scss");
 
 
 /***/ })
