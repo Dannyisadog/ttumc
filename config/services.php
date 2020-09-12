201193fedc06d319d8c227e8f8a75e1bcaf79795
Dannyisadog <dannyisadog10@gmail.com>
2020-09-12 12:38:58 +0800

    feat: mailgun setting

--- config/services.php
+++ config/services.php
@@ -15,7 +15,7 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('ttu-mc.comå'),
+        'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
