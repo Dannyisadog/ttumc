c18147272a3767ccbdd1b8b47d615df61c84dbba
Dannyisadog <dannyisadog10@gmail.com>
2020-09-12 15:36:12 +0800

    feat: remove unuse code

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -8,29 +8,12 @@ class MailController extends Controller
 {
     public function sendTestMail()
     {
-        $from = [
-            'email' => 'ttumc@ttumc.dannyisadog.com',
-            'name' => 'ttumc',
-            'subject' => 'test mail'
-        ];
-
-        $to = [
-            'email' => 'dannyisadog10@gmail.com',
-            'name' => 'Danny'
-        ];
-
-        $data = [
-            'subject' => "test mail"
-        ];
-
-        $mail = new Mail;
-
-        $mail::send('emails.test', ['key' => 'value'], function ($message) {
+        Mail::send('emails.test', ['key' => 'value'], function ($message) {
             $message->to('dannyisadog10@gmail.com', 'Danny')->subject('test email!');
         });
 
-        if ($mail::failures()) {
-            print_r($mail::failures());
+        if (Mail::failures()) {
+            print_r(Mail::failures());
         } else {
             echo "成功";
         }
