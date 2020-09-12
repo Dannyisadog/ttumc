b38623e1bfaa1c4822948f54bda5e051196ba5d5
Dannyisadog <dannyisadog10@gmail.com>
2020-09-12 16:37:09 +0800

    feat: named reset password route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::post('register', 'Auth\RegisterController@register');
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 // Home Route
 Route::get('/home', 'ScheduleController@showSchedule')->name('home');
