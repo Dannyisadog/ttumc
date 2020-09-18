80e8d6edfc2bdd803fca31e8c638a7f1474a85c8
Dannyisadog <dannyisadog10@gmail.com>
2020-09-18 23:09:22 +0800

    adjust: band list column width style

--- resources/views/bandlist.blade.php
+++ resources/views/bandlist.blade.php
@@ -7,20 +7,20 @@
         <table class="table table-dark table-bordered">
             <thead>
               <tr>
-                <th scope="col" width="10%">#</th>
-                <th scope="col" width="70%">團名</th>
-                <th scope="col" width="10%">團長</th>
-                <th scope="col" width="10%">動作</th>
+                <th scope="col" width="10%" class="band-index-title">#</th>
+                <th scope="col" width="40%" class="band-name-title">團名</th>
+                <th scope="col" width="20%" class="band-lead-title">團長</th>
+                <th scope="col" width="10%" class="band-action-title">動作</th>
               </tr>
             </thead>
             <tbody>
             <?php $count = 1; ?>
             @foreach ($bands as $band)
-                <tr>
-                    <td>{{$count++}}</td>
-                    <td>{{$band->name}}</td>
-                    <td>{{$band->leader->name}}</td>
-                    <td>
+                <tr class="band-rows" height="62px">
+                    <td class="band-index">{{$count++}}</td>
+                    <td class="band-name">{{$band->name}}</td>
+                    <td class="band-lead">{{$band->leader->name}}</td>
+                    <td class="band-action">
                       @if (isset($band_join_map) && !$band_join_map[$band->id])
                         {{Form::open(array('method'=>'post','route' => 'joinBand'))}}
                         {{Form::hidden('user_id', Auth::user()->id)}}
