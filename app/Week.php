3aff15dde1d999589f2eaca525f7d7369323ed61
Dannyisadog <dannyisadog10@gmail.com>
2020-09-19 17:42:49 +0800

    fix: add time to week start and end time

--- app/Week.php
+++ app/Week.php
@@ -9,8 +9,8 @@ class Week extends Model
     public static function getWeekRange()
     {
         $day = date('w');
-        $week_start = date('Y-m-d', strtotime('monday this week'));
-        $week_end = date('Y-m-d', strtotime('monday this week') + 86400 * 6);
+        $week_start = date('Y-m-d 00:00:00', strtotime('monday this week'));
+        $week_end = date('Y-m-d 23:59:59', strtotime('monday this week') + 86400 * 6);
 
         return [$week_start, $week_end];
     }
