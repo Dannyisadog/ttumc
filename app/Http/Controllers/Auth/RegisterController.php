c5f83d1dc33b67b4b35fa52e5713e1b0114dc32c
Dannyisadog <dannyisadog10@gmail.com>
2020-10-25 00:46:51 +0800

    fix: register confirmation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'ttumc_confirmation' => ["required" , "max:3", "regex:(柯庭堯)"]  
         ]);
     }
 
