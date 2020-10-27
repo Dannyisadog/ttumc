d7b000f46bfe6dfe67a0645843167d110e042059
Dannyisadog <dannyisadog10@gmail.com>
2020-10-28 01:51:21 +0800

    feat: black shortcut icon

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
     <!-- favicon -->
-    <link rel="shortcut icon" type="image/png" href="{{ url('images/logo/logo_white.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ url('images/logo/logo_black.png') }}">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
