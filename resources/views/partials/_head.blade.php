c5492db8c509e645ec95cd2f1bfa5e7b272bb61c
Danny <dannyisadog10@gmail.com>
2021-12-01 23:32:17 +0800

    fix: font cdn link to https

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -23,7 +23,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="http://fonts.cdnfonts.com/css/sandora" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/sandora" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
     <!-- favicon -->
